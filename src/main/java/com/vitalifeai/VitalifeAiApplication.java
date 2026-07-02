66d464d872312d396e975085e916d26c4f2954b2
wutyihlaingwin <x24189146@student.ncirl.ie>
2026-07-02 23:49:39 +0100

    Add Spring Boot project source structure

---
+++ src/main/java/com/vitalifeai/VitalifeAiApplication.java
@@ -0,0 +1,13 @@
+package com.vitalifeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalifeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalifeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitalifeai/VitalifeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalifeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalifeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
